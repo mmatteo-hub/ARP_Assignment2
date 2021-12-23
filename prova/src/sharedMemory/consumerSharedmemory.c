28342d56d3ffc72db3fe49446ef5f685599a8dbc
MonicaF99 <monica.fossati99@gmail.com>
2021-12-23 09:51:45 +0100

    shared mem

--- prova/src/sharedMemory/consumerSharedmemory.c
+++ prova/src/sharedMemory/consumerSharedmemory.c
@@ -10,38 +10,68 @@
 #define SHMOBJ_PATH "/shm_AOS"
 #define SEM_PATH_1 "/sem_AOS_1"
 #define SEM_PATH_2 "/sem_AOS_2"
+#define SEM_PATH_3 "/sem_AOS_3"
 
 
 char dum[10];
 
 int main(int argc, char *argv[]) {
   int dim = atoi(argv[1]);
+  int dim_buff = dim/1024;
   char B[dim];
-  int shared_seg_size = (sizeof(B));
+  int shared_seg_size = (dim_buff);
   char *ptr;
   int j;
+  printf("111");
+  fflush(stdout);
   int shmfd = shm_open(SHMOBJ_PATH, O_RDONLY, 0666);
   ptr = mmap(NULL, shared_seg_size, PROT_READ, MAP_SHARED, shmfd, 0);
-  sem_t * sem_id1 = sem_open(SEM_PATH_1, 0);
-  sem_t * sem_id2 = sem_open(SEM_PATH_2, 0);
+  sem_t * NotFull = sem_open(SEM_PATH_1, 0);
+  sem_t * Mutex = sem_open(SEM_PATH_2, 0);
+  sem_t * NotEmpty = sem_open(SEM_PATH_3, 0);
 
-  // repeat 3 times
+  /*// repeat 3 times
   for(j=0;j<3;j++) {
     // wait prod
     sem_wait(sem_id2);
-    /* get shared data */
+    printf("111");
+    fflush(stdout);
+    /* get shared data
     printf("%s\n", ptr);
-    printf("enter anything to proceed "); fflush(stdin);
-    scanf("%s", dum);
+    fflush(stdout);
+    scanf("%s", B);
+    printf("222");
+    printf("%s\n", B);
+    fflush(stdout);
     // start prod
     sem_post(sem_id1);
+  }*/
+  
+  printf("Before consuming\n");
+  fflush(stdout);
+  int out = 0;
+  for(int i = 0; i < dim; i++)
+  {
+  	sem_wait(NotEmpty);
+  	sem_wait(Mutex);
+  	B[i] = ptr[out];
+  	out = (out + 1) % dim_buff;
+  	sem_post(Mutex);
+  	sem_post(NotFull);
+  	printf("r");
+  	fflush(stdout);
   }
+  
+  printf("%s", B);
+  fflush(stdout);
 
   shm_unlink(SHMOBJ_PATH);
-  sem_close(sem_id1);
-  sem_close(sem_id2);
+  sem_close(NotFull);
+  sem_close(NotEmpty);
+  sem_close(Mutex);
   sem_unlink(SEM_PATH_1);
   sem_unlink(SEM_PATH_2);
+  sem_unlink(SEM_PATH_3);
 
   return 0;
 }
