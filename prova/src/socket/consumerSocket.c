a577a1878764a47727256870a1d855289fc6f771
MonicaF99 <monica.fossati99@gmail.com>
2021-12-28 18:56:36 +0100

    socket

--- prova/src/socket/consumerSocket.c
+++ prova/src/socket/consumerSocket.c
@@ -1,92 +1,84 @@
-#include <sys/socket.h>
-#include <sys/types.h>
-#include <netinet/in.h>
-#include <netdb.h>
 #include <stdio.h>
-#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h> 
 #include <stdlib.h>
+#include <strings.h>
+#include <string.h>
 #include <unistd.h>
-#include <signal.h>
-#include <errno.h>
-#include <arpa/inet.h> 
-#include <sys/time.h> 
-#include <time.h> 
 
 #define MEGA 1048576
 #define X 1024
 
+void error(char *msg)
+{
+    perror(msg);
+    exit(0);
+}
+
 int main(int argc, char *argv[])
 {
-    // 
-    int sockfd = 0, n = 0;
-    struct sockaddr_in serv_addr;
+    int sockfd, portno, n;
 
-    if(argc != 4)
-    {
-        printf("\n Usage: %s <ip of server> \n",argv[0]);
-        return 1;
-    }
+    struct sockaddr_in serv_addr;
+    struct hostent *server;
     
-    // dimension taken from master: already casted to length for the array of integers
     int dim = atoi(argv[2]);
     int number = dim * MEGA;
     // number of kB
     int y = dim*X;
-    // get pid producer process
-    pid_t pid_prod = atoi(argv[3]);
-
+    
     // intialising the array for the consumer process
     char* B;
     B = (char *) malloc(sizeof(char)*number);
     if(B == NULL)
         perror("Run out of memory\n");
-       
-    // send signal
-    kill(pid_prod, SIGUSR2);
-    
 
-    memset(B, '0',sizeof(B));
-    if((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
-    {
-        printf("\n Error : Could not create socket \n");
-        return 1;
-    } 
-
-    memset(&serv_addr, '0', sizeof(serv_addr)); 
-
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_port = htons(5000); 
-
-    if(inet_pton(AF_INET, argv[1], &serv_addr.sin_addr)<=0)
-    {
-        printf("\n inet_pton error occured\n");
-        return 1;
-    } 
-
-    if( connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
-    {
-       printf("\n Error : Connect Failed \n");
-       kill(pid_prod, SIGKILL);
-       return 1;
+    char buffer[256];
+    if (argc < 4) {
+       fprintf(stderr,"usage %s hostname port\n", argv[0]);
+       exit(0);
     }
-    
-    // send signal
-    kill(pid_prod, SIGUSR2);
+    portno = atoi(argv[3]);
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0) 
+        error("ERROR opening socket");
+    server = gethostbyname(argv[1]);
+    if (server == NULL) {
+        fprintf(stderr,"ERROR, no such host\n");
+        exit(0);
+    }
+    bzero((char *) &serv_addr, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    bcopy((char *)server->h_addr, 
+         (char *)&serv_addr.sin_addr.s_addr,
+         server->h_length);
+    serv_addr.sin_port = htons(portno);
+    if (connect(sockfd,(struct sockaddr *)&serv_addr,sizeof(serv_addr)) < 0) 
+        error("ERROR connecting");
+    bzero(buffer,256);
+    sprintf(buffer, "%s", "Ready");
+    n = write(sockfd,buffer,strlen(buffer));
+    if (n < 0) 
+         error("ERROR writing to socket");
+    bzero(B, number);
     
     for(int i=0; i<y;i++)
     {
         // read from fd[0]
-        read(sockfd, &B[i*X], sizeof(char)*X);
+        n = read(sockfd, &B[i*X], sizeof(char)*X);
     }
 
     printf("Last 20 vals of B = %s\nDim(B)=%ld\n",&B[number-20],strlen(B));
     fflush(stdout);
-
+    if (n < 0) 
+         error("ERROR reading from socket");
+    sprintf(buffer, "%s", "Done");
+    n = write(sockfd,buffer,strlen(buffer));
+    if (n < 0) 
+         error("ERROR writing to socket");
     close(sockfd);
-    
-    // send signal
-    kill(pid_prod, SIGUSR1);
     free(B);
-
     return 0;
 }
